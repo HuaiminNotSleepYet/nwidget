78d39eaa3689c3b2b2260dd938bc0f9200067da5
ZhangHuaimin <HuaiminNotSleepYet@outlook.com>
2024-08-13 16:20:34 +0800

    feat: add missing property for Frame.

--- include/nwidget/frame.h
+++ include/nwidget/frame.h
@@ -14,6 +14,13 @@ class FrameBuilder : public WidgetBuilder<S, T>
 
 public:
     using WidgetBuilder<S, T>::WidgetBuilder;
+
+    S& frameStyle(int style)                { t->setFrameStyle(style)  ; return self(); }
+    S& frameShape(QFrame::Shape shape)      { t->setFrameShape(shape)  ; return self(); }
+    S& frameShadow(QFrame::Shadow shadow)   { t->setFrameShadow(shadow); return self(); }
+    S& lineWidth(int width)                 { t->setLineWidth(width)   ; return self(); }
+    S& midLineWidth(int width)              { t->setMidLineWidth(width); return self(); }
+    S& frameRect(const QRect& rect)         { t->setFrameRect(rect)    ; return self(); }
 };
 
 N_BUILDER_IMPL(FrameBuilder, QFrame, Frame);
